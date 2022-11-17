038e8cdf16b71186dc45169f61c64e29208d593d
Harmen <harmenschouten@outlook.com>
2022-11-17 22:05:36 +0100

    Day 6 - Part 1

---
+++ Day6/Part1.ts
@@ -0,0 +1,28 @@
+const text = await Deno.readTextFile("./Day6/input.txt")
+
+const lines = text.split("\r\n")
+
+const line = lines[0].trim()
+
+let fish = line.split(',')
+
+console.log(fish.length)
+
+for(let i = 0; i < 80; i++) {
+    const copyFish = [...fish]
+    fish.forEach((item, index) => {
+        switch(Number(item)) {
+            case 0: 
+                copyFish.splice(index, 1, '6')
+                copyFish.push('8')
+                break;
+            default:
+                copyFish.splice(index, 1, (Number(item) - 1).toString())
+                break;
+        }
+    })
+
+    fish = copyFish
+}
+
+console.log(fish.length)
