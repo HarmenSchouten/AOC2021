b1721cb77a2cb073ba949b8023fbc97ba5ff0f9b
Harmen <harmenschouten@outlook.com>
2022-11-19 11:24:29 +0100

    Comment unused stuff

--- Day8/Part2.ts
+++ Day8/Part2.ts
@@ -1,7 +1,7 @@
 const text = await Deno.readTextFile("./Day8/input.txt")
 const lines = text.split("\r\n")
 
-const CombineDistinct = (a:string, b:string):string => [...(a + b)].reduce((acc, state) => [...acc].includes(state) ? acc : acc + state, '');
+//const CombineDistinct = (a:string, b:string):string => [...(a + b)].reduce((acc, state) => [...acc].includes(state) ? acc : acc + state, '');
 
 const Minus = (a: string, b:string):string => [...a].reduce((acc, state) => [...b].includes(state) ? acc : acc + state, '')
 
@@ -26,15 +26,15 @@ console.log("TOTALS", lines.reduce((acc, line) => {
     const cinqs = signalPatterns.filter(item => item.length === 5)
     
     // Deductions
-    const a = Minus(sette, uno)
+    //const a = Minus(sette, uno)
     const nuvo = seis.filter(item => [...quattro].every(char => [...item].includes(char)))[0]
     const e = Minus(otto, nuvo)
     const sei = seis.filter(item => item !== nuvo && cinqs.filter(cinq => [...cinq].every(char => [...Minus(item, e)].includes(char))).length > 0)[0]
     const zero = seis.filter(item => item !== nuvo && item !== sei)[0]
-    const d = Minus(otto, zero)
-    const g = Minus(zero, CombineDistinct(CombineDistinct(quattro, a), e))
+    //const d = Minus(otto, zero)
+    //const g = Minus(zero, CombineDistinct(CombineDistinct(quattro, a), e))
     const cinq = cinqs.filter(item => [...(item + e)].every(char => [...sei].includes(char)))[0]!
-    const c = Minus(otto, sei)
+    //const c = Minus(otto, sei)
     const due = cinqs.filter(item => item !== cinq && [...item].includes(e))[0]
     const tres = cinqs.filter(item => item !== due && item !== cinq)[0]
     
