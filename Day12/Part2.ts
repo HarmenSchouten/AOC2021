d1b9d96320b79fe445287eb9cefc97c8855749b1
Harmen <harmenschouten@outlook.com>
2022-11-21 21:00:30 +0100

    Day 12; Part 2

---
+++ Day12/Part2.ts
@@ -0,0 +1,66 @@
+const text = await Deno.readTextFile("./Day12/input.txt")
+const lines = text.split("\r\n")
+
+const input = lines.reduce((acc, line) => [...acc, line.split('-')], [] as string[][])
+
+const starts = input.filter(line => line.includes("start")).map(item => item.sort((a, b) => b.length - a.length))
+const rest = input.filter(line => !line.includes("start"))
+
+function GetNextPathSections(start:string[]) {
+    const lastItem = start[start.length - 1]
+
+    if (lastItem !== 'end') {
+        const matchingItems = rest.filter(item => item[0] === lastItem || item[1] === lastItem)
+
+        return matchingItems.reduce((acc, matchingItem) => {
+
+            const item = getItem(lastItem, matchingItem)
+            if (isLowerCase(item) && hasItem(item, start)) {
+                return [...acc]
+            } else {
+                return [...acc, [...start, item]]
+            }
+        }, [] as string[][])
+    }
+}
+
+function getItem(check:string, match:string[]) {
+    return match[0] === check ? match[1] : match[0]
+}
+
+function isLowerCase(char:string) {
+    return char === char.toLowerCase()
+}
+
+function hasItem(char:string, input:string[]) {
+    const lowerCaseInputs = input.filter(item => item !== "start" && item !== "end" && isLowerCase(item))
+    const hasDuplicates = new Set(lowerCaseInputs).size !== lowerCaseInputs.length
+    if (hasDuplicates) {
+        return input.includes(char)
+    }
+    return hasDuplicates
+}
+
+const possiblePaths = starts.reduce((acc, start) => {
+    let paths:string[][] = [[...start]]
+
+    while(!paths.every(path => path[path.length - 1] === "end")) {
+        const open = paths.filter(item => item[item.length - 1] !== "end")
+        const closed = paths.filter(item => item[item.length - 1] === "end")
+
+        paths = [
+            ...closed,
+            ...open.reduce((acc, path) => {
+                const next = GetNextPathSections(path)
+                if (next !== undefined) {
+                    return [...acc, ...next]
+                }
+                return acc
+            }, [] as string[][])
+        ]
+    }
+
+    return [...acc, ...paths]
+}, [] as string[][])
+
+console.log("NUMBER OF PATHS", possiblePaths.length)
